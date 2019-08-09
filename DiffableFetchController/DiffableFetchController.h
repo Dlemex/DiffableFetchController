2d478a36d4faef3d2ef2b3dc989caa051f2bd6a3
David Edwards <dle@tech4tomorrow.net>
2019-08-09 11:10:45 -0600

    Initial Commit

---
+++ DiffableFetchController/DiffableFetchController.h
@@ -0,0 +1,19 @@
+//
+//  DiffableFetchController.h
+//  DiffableFetchController
+//
+//  Created by David Edwards on 8/9/19.
+//  Copyright © 2019 Tech For Tomorrow, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiffableFetchController.
+FOUNDATION_EXPORT double DiffableFetchControllerVersionNumber;
+
+//! Project version string for DiffableFetchController.
+FOUNDATION_EXPORT const unsigned char DiffableFetchControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiffableFetchController/PublicHeader.h>
+
+
